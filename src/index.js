f0e2ba461e0e6eb14b01b795e0fb72b534b3486b
gaurav-techies <gauravkamboj13121992@gmail.com>
2020-05-26 11:01:20 +0530

    project_setup_season_display

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component{
+    //This is only time we are direct assignment to the state as this.state 
+    state ={lat:null,errorMessage:''};  //This is equilant to same as we define in constructor
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat:position.coords.latitude}), //we called setstate from react component
+            err=>this.setState({errorMessage:err.message})
+        );
+    }
+    componentDidUpdate(){
+        console.log("component did update will work when state will get updated")
+    }
+    componentWillMount(){
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    }
+    //React says we have to define render 
+    render() {
+                if(!this.state.errorMessage && this.state.lat){
+                   return <SeasonDisplay lat={this.state.lat} />
+                }
+                
+                if(this.state.errorMessage && !this.state.lat){
+                    return  <div> Error: {this.state.errorMessage}</div>;
+                }
+
+                //here it is outside because by default loading will come until dom will rerender 
+                return <div>Loading!</div>
+    }
+}
+ReactDom.render(
+    <App/>,document.querySelector("#root")
+)
